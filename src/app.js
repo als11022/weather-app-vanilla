9ce47d0ac42f83bbede43ddbe265d67cd4ffbe8f
als11022 <95195221+als11022@users.noreply.github.com>
2022-01-07 11:43:14 -0500

    Updated search engine
    added styles, added current location button. Still have issue with bootstrap loading style not disappearing until page is refreshed

--- src/app.js
+++ src/app.js
@@ -13,14 +13,14 @@ let day = days[date.getDay()];
 return `${day}, ${hours}:${minutes}`
 }
 
-function displayTemperature (response){
+function retrieveCurrentData (response){
     //console.log(response);
     fahrenheitTemp = response.data.main.temp;
     document.querySelector("#temperature").innerHTML = Math.round(fahrenheitTemp);
     document.querySelector("#city").innerHTML = response.data.name;
     document.querySelector("#description").innerHTML = response.data.weather[0].description;
-    document.querySelector("#humidity").innerHTML = response.data.main.humidity;
     document.querySelector("#feels-like").innerHTML = Math.round(response.data.main.feels_like);
+    document.querySelector("#humidity").innerHTML = response.data.main.humidity;
     document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
     document.querySelector("#date").innerHTML = formatDate(response.data.dt * 1000);
     document.querySelector("#icon").setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
@@ -29,14 +29,41 @@ function displayTemperature (response){
 
 function search(city){
     let apiKey = "a50f410ea36ad12d8cb30de68e6fc33b";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
-    axios.get(apiUrl).then(displayTemperature);
+    units = "imperial";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+    axios.get(apiUrl).then(retrieveCurrentData);
 
 }
 function handleSubmit(event){
     event.preventDefault();
-    let cityInputText = document.querySelector("#city-input");
-    search(cityInputText.value);
+    let cityInputText = document.querySelector("#city-input").value;
+    if (cityInputText.trim()) {
+        search(cityInputText);
+    }
+    else {
+        document.querySelector("#city").innerHTML = "Unknown";
+        document.querySelector("#temperature").innerHTML = null;
+        document.querySelector("#description").innerHTML = null;
+        document.querySelector("#feels-like").innerHTML  = null;
+        document.querySelector("#humidity").innerHTML  = null;
+        document.querySelector("#wind").innerHTML  = null;
+        document.querySelector("#icon").setAttribute("src","data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs%3D");
+        document.querySelector("#icon").setAttribute("class", "spinner-border");
+        document.querySelector("#icon").setAttribute("alt",  " ");
+        alert("Please enter a location");
+    }
+}
+
+function retrieveGpsLocation (response) {
+    let apiKey ="a50f410ea36ad12d8cb30de68e6fc33b";
+    let units ="imperial";
+    let lat = response.coords.latitude;
+    let lon = response.coords.longitude;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${units}&appid=${apiKey}`;
+    axios.get(apiUrl).then(retrieveCurrentData);
+}
+function getCurrentPosition (){
+    navigator.geolocation.getCurrentPosition(retrieveGpsLocation);
 }
 
 function convertToCelsius (event){
@@ -60,6 +87,9 @@ let fahrenheitTemp = null;
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+let button = document.querySelector("#current-button");
+button.addEventListener("click", getCurrentPosition);
 
 let celsiusLink = document.querySelector("#c-link");
 celsiusLink.addEventListener("click", convertToCelsius);
